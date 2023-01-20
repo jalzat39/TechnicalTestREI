6c9daa6a88aa0409c1ab103e14ff1e7a8b81d1b5
jalzat39 <jmiguelalzate@gmail.com>
2023-01-20 15:26:16 -0500

    Adding Dictionary and OnlyDictionary

--- src/main/java/code/Dictionary.java
+++ src/main/java/code/Dictionary.java
@@ -1,4 +1,12 @@
 package code;
 
+import utility.OnlineDictionary;
+
 public class Dictionary {
+    public static boolean isEnglishWord(String word){
+        if(OnlineDictionary.wordsSet.contains(word)){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/utility/OnlineDictionary.java
+++ src/main/java/utility/OnlineDictionary.java
@@ -1,4 +1,12 @@
 package utility;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class OnlineDictionary {
+
+    private static String[] words = {};
+
+    public static Set<String> wordsSet= new HashSet<String>(Arrays.asList(words));
 }
