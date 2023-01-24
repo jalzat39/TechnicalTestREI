520bc841006c10769e5e2a91ad890fa1363869de
jalzat39 <jmiguelalzate@gmail.com>
2023-01-24 08:59:21 -0500

    Removing duplicate test cases and improving other test cases

--- src/test/java/test/Tests.java
+++ src/test/java/test/Tests.java
@@ -15,75 +15,45 @@ public class Tests {
     }
 
     @Test
-    public void testContainLastWord() {
-        Assert.assertTrue(search.covertWord("ilikesamsung").contains("sung"));
-    }
-
-    /**
-     * Second test, which checks, given a word in English and may contain within
-     * it another word in English and this located at the beginning of the word.
-     */
-    @Test
-    public void testContainFirstWord() {
-        Assert.assertTrue(search.covertWord("ilikesamsung").contains("like"));
-    }
-
-    /**
-     * Third test, check that, given a word in English, to be able to show the words
-     * in English that it contains.
-     */
-    @Test
-    public void testShowWordsWorking() {
+    public void testInvertedWord() {
         Assert.assertTrue(search.covertWord("working").contains("row"));
     }
 
-    /**
-     * Forth test, check that, given a word in English, you can find other words in
-     * English by changing the order of the letters.
-     */
     @Test
     public void testWordsExchanged() {
         Assert.assertTrue(search.covertWord("working").contains("working"));
     }
 
-    /**
-     * Fifth test, validate that, given a word in English and within it the same word
-     * in English can be twice, only save one word in English.
-     */
     @Test
     public void testDontRepeatWords() {
         Assert.assertEquals(1, search.covertWord("workingring").stream().filter(i -> i.equals("ring")).count());
     }
 
-    /**
-     * Sixth test, validate that, when a word in English does not appear in the dictionary,
-     * it marks it as false.
-     */
     @Test
     public void testWordDoesntAppear() {
         Assert.assertTrue(search.covertWord("banana").isEmpty());
     }
 
-    /**
-     * Seven test, check that, given a very long English word, the system can respond quickly
-     */
     @Test(timeout = 5000)
     public void testLargeWords() {
         Assert.assertTrue(search.covertWord("workingRingWorkingRingWorkingRing").contains("ring"));
     }
 
-    /**
-     * Eighth test, validate that, given a word in English with changes in uppercase and lowercase
-     * letters, the program can work correctly.
-     */
     @Test
     public void testUpperLowerWords() {
         Assert.assertTrue(search.covertWord("WoRKiNg").contains("ring"));
     }
 
-    /**
-     * Ninth test, validate that, given a word in English that is empty.
-     */
+    @Test
+    public void testOnlyLetters(){
+        try {
+            Assert.assertFalse(search.covertWord("Hello123").isEmpty());
+        } catch (Exception e) {
+            System.out.println(e);
+            Assert.assertEquals("java.lang.RuntimeException: The word only have alphabet character", e.toString());
+        }
+    }
+
     @Test
     public void testEmptyWords() {
         try {
@@ -94,16 +64,8 @@ public class Tests {
         }
     }
 
-    /**
-     * Tenth test, validate that, given a word in English with blank space.
-     */
     @Test
-    public void testBlankSpace() {
-        try {
-            Assert.assertFalse(search.covertWord("Working Ring").isEmpty());
-        } catch (Exception e) {
-            System.out.println(e);
-            Assert.assertEquals("java.lang.RuntimeException: The word only have alphabet character", e.toString());
-        }
+    public void testWithsRepeatLetters(){
+        Assert.assertTrue(search.covertWord("cartoon").contains("cartoon"));
     }
 }
